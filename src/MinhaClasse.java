5cee53c34e1ec1e779b0b1669fb1992918319b7b
Joao Pedro <canadajoaoalmeida@gmail.com>
2024-04-26 01:37:29 -0300

    update

---
+++ src/MinhaClasse.java
@@ -0,0 +1,16 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+     
+        String primeiroNome = "joão";
+    String segundoNome= "Pedro";
+    String nomeCompleto="nomeCompleto" (primeiroNome,segundoNome);
+    System.out.println(nomeCompleto);
+    }
+public static String nomeCompleto (String primeiroNomem; String segundoNome)
+{ return primeiroNome.concat(" ").concat(segundoNome);
+}
+
+        
+    
+
